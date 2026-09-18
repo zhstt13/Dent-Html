f5400c6395f33ce25e37aca9e758df3ffa7cb5b9
zhstt13 <zhstt13@gmail.com>
2026-09-19 02:45:41 +0330

    Add component loader foundation

---
+++ src/scripts/app.js
@@ -0,0 +1,12 @@
+// Dent HTML component runtime foundation
+
+const components = [
+  'header',
+  'hero',
+  'navigation',
+  'flashcard',
+  'lecture',
+  'quiz'
+];
+
+console.log('Dent HTML initialized', components);
