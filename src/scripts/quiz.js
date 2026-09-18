b787cd6bef27c0686f0d4dbd84276cc9a3ea15e2
zhstt13 <zhstt13@gmail.com>
2026-09-19 02:53:07 +0330

    Add quiz controller foundation

---
+++ src/scripts/quiz.js
@@ -0,0 +1,12 @@
+// Dent HTML Quiz Controller Foundation
+
+const quizController = {
+  init() {
+    console.log('Quiz controller initialized');
+  },
+  checkAnswer(question, answer) {
+    return { question, answer };
+  }
+};
+
+export default quizController;
